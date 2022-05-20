99130675ae847f84b5f86d3c36be1ddf07b6b65b
Thibault Hamel <contact@adaedra.eu>
2022-05-20 22:04:42 +0200

    Re-indent base.nix and change nix version

--- modules/base.nix
+++ modules/base.nix
@@ -1,48 +1,48 @@
 { name }: { pkgs, ... }: {
-    system.stateVersion = "21.11";
+  system.stateVersion = "21.11";
 
-    nix.package = pkgs.nixFlakes;
-    nix.extraOptions = ''
-        experimental-features = nix-command flakes
+  nix.package = pkgs.nix_2_7;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  programs.zsh = {
+    enable = true;
+    interactiveShellInit = ''
+      bindkey -e
+    '';
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+  hardware.enableRedistributableFirmware = true;
+  hardware.cpu.intel.updateMicrocode = true;
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  services.openssh.enable = true;
+  services.openssh.extraConfig = ''
+    ClientAliveInterval 60
+  '';
+
+  networking.hostName = name;
+  networking.firewall.enable = false;
+
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+  programs.neovim.configure = {
+    customRC = ''
+      set bg=dark
+      set number
+      set sw=4 sts=4 et ai
     '';
 
-    programs.zsh = {
-        enable = true;
-        interactiveShellInit = ''
-            bindkey -e
-        '';
+    packages.system = with pkgs.vimPlugins; {
+      start = [ vim-nix ];
     };
-    users.defaultUserShell = pkgs.zsh;
+  };
 
-    hardware.enableRedistributableFirmware = true;
-    hardware.cpu.intel.updateMicrocode = true;
-
-    boot.loader = {
-        systemd-boot.enable = true;
-        efi.canTouchEfiVariables = true;
-    };
-
-    services.openssh.enable = true;
-    services.openssh.extraConfig = ''
-        ClientAliveInterval 60
-    '';
-
-    networking.hostName = name;
-    networking.firewall.enable = false;
-
-    programs.neovim.enable = true;
-    programs.neovim.defaultEditor = true;
-    programs.neovim.configure = {
-        customRC = ''
-            set bg=dark
-	    set number
-	    set sw=4 sts=4 et ai
-	'';
-
-	packages.system = with pkgs.vimPlugins; {
-            start = [ vim-nix ];
-	};
-    };
-
-    programs.htop.enable = true;
+  programs.htop.enable = true;
 }
